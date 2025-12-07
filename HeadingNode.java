cba2d2eba0dc3cdd762d19b1137bb9aa15f53317
Hasan BAHÇECİ <hasan.bahceci021@gmail.com>
2025-12-07 17:25:23 +0300

    modified

--- HeadingNode.java
+++ HeadingNode.java
@@ -1,3 +1,47 @@
 public class HeadingNode {
-    
+
+    private String title;
+    private HeadingNode parent;
+    private HeadingNode next;
+    private HeadingNode child;
+
+    public HeadingNode(String title)
+    {
+        this.title = title;
+        this.parent = null;
+        this.child = null;
+        this.next = null;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public HeadingNode getParent() {
+        return parent;
+    }
+
+    public void setParent(HeadingNode parent) {
+        this.parent = parent;
+    }
+
+    public HeadingNode getNext() {
+        return next;
+    }
+
+    public void setNext(HeadingNode next) {
+        this.next = next;
+    }
+
+    public HeadingNode getChild() {
+        return child;
+    }
+
+    public void setChild(HeadingNode child) {
+        this.child = child;
+    }
 }
--- LinkedBook.java
+++ LinkedBook.java
@@ -1,3 +1,36 @@
 public class LinkedBook {
-    
+
+    private HeadingNode head;
+
+    public LinkedBook()
+    {
+        head = null;
+    }
+
+    public void add(String path, String title)
+    {
+
+    }
+
+    public void print()
+    {
+
+    }
+
+    private int[] parsePath(String path)
+    {
+        return null;
+        
+    }
+
+    private HeadingNode findNodeByPath(String path)
+    {
+        return head;
+        
+    }
+
+    private void printRecursive(HeadingNode node, String prefix)
+    {
+        
+    }
 }
